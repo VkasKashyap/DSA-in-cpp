529c406e26664d123121547dac8f8ed3cc72b55e
VIkas Kashyap <vikas965046@gmail.com>
2026-01-17 21:01:11 +0530

    Update GCDofTwoNumbers.cpp

--- Codes/Questions/Basic Recursion/GCDofTwoNumbers.cpp
+++ Codes/Questions/Basic Recursion/GCDofTwoNumbers.cpp
@@ -6,10 +6,7 @@ int GCD(int a, int b){
     if(b==0)    return a;
     return GCD(b,a%b);     
 }
-/*
-The gcd function uses the Euclidean algorithm, 
-which states that the GCD of two numbers a and b is the same as the GCD of b and a % b.
-*/
+
 
 int main(){
     int a,b;
